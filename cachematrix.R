bbfdfb556503f6c3d12ea1668a9d59aa271de1d0
Serg C <about.r.wiz@gmail.com>
2017-05-19 11:45:25 -0400

    modified

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This module consists of two functions
+##
+## makeCacheMatrix
+##  The function to create a container to store two matrix objects:
+##   a) mtx - the source matrix
+##   b) inv - the object to cache the inverse of mtx.
+##  This function also provides get/set access methods to manage stored objects.
+##
+##  cacheSolve:
+##   This function computes the inverse of the matrix 'mtx' stored by 
+##   makeCacheMatrix above. If the inverse has already been calculated
+##  ('inv' is not reset/NULL), then cacheSolve retrieves the inverse
+##   from the cache rather than calculate it again.
+##
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+        ## 1/ container for: 'mtx'-a source matrix, 'inv'-the inverse of 'mtx'
+        ## 2/ initialize both: mtx, inv
+        ## 3/ return list of methods to access 'mtx', 'inv'
+        set <- function(x) {
+                mtx <<- x
+                inv <<- NULL
+        }
+        setinv <- function(i)inv <<- i
+        getinv <- function() inv
+        get    <- function() mtx
+        set(x)
+	      list( set = set,
+              get = get,
+              setinv = setinv,
+              getinv = getinv )
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## 1/ verify if the inverse was reset
+        ## 2/ if need be - calculate, store the new inverse 
+        ## 3/ return the inverse matrix (from cache or calculated)
+        inv <- x$getinv()
+        if( !is.null(inv) ) { 
+        ## 'not reset' - the copy from container
+                message("from cache...")
+        }
+	      else { 
+        ## 'reset' - get the new source, calculate and store new inverted
+                a <- x$get()
+                inv <- solve(a)
+                x$setinv(inv)
+	      }
+        inv
 }
